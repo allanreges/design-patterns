0b8bdfb9ab6727172fcc6f1dd57e971c3978c088
allanreges <allan_creative@hotmail.com>
2021-04-25 12:05:10 -0300

    day 2

--- apps/meso/src/app/home/home.component.ts
+++ apps/meso/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concat } from 'rxjs';
 
 interface BaseEntity {
   id: string | null
@@ -9,6 +10,12 @@ interface Client extends BaseEntity {
   lastName: string;
   company: string;
 }
+
+interface Action {
+  type: string;
+  payload?: any;
+}
+
 
 const peter: Client = {
   id: '1',
@@ -45,6 +52,59 @@ const initialClientState: ClientsState = {
   clients,
   currentClient: newClient
 }
+
+class ClientStore {
+  state: ClientsState;
+
+  constructor(state: ClientsState) {
+    this.state = state
+  }
+
+  getState() {
+    return this.state
+  }
+
+  select(key: string) {
+    this.state[key]
+  }
+}
+
+const CLIENT_LOAD    = '[Client] Load';
+const CLIENT_CREATE  = '[Client] Create';
+const CLIENT_UPDATE  = '[Client] Update';
+const CLIENT_DELETE  = '[Client] Delete';
+const CLIENT_SELECT  = '[Client] Select';
+const CLIENT_CLEAR   = '[Client] Clear';
+
+const loadClients = (state, clients) => {
+  console.log('LOAD CLIENTS!', clients)
+  return state
+}
+
+const selectClient = (state, client) => {
+  console.log('LOAD CLIENT!', client)
+  return state
+}
+
+
+const clientsReducer = (state: ClientsState = initialClientState, action: Action) => {
+  switch(action.type) {
+    case CLIENT_LOAD:
+      return loadClients(state, action);
+    case CLIENT_SELECT: 
+      return selectClient(state, action.payload);
+    default:
+      return state;
+  }
+}
+
+const clientsStore = new ClientStore(initialClientState)
+const currentClient = clientsStore.select('currentClient')
+
+//clientsStore.load(clients)
+//clientsStore.select(peter)
+
+
 
 interface Project extends BaseEntity {
   title: string;
@@ -84,10 +144,99 @@ const projects: Project[] = [
 ]
 
 
-const initialProjectState: ProjectsState = {
+const initialProjectsState: ProjectsState = {
   projects,
   currentProject: newProject
 }
+
+const PROJECT_LOAD    = '[Project] Load';
+const PROJECT_CREATE  = '[Project] Create';
+const PROJECT_UPDATE  = '[Project] Update';
+const PROJECT_DELETE  = '[Project] Delete';
+const PROJECT_SELECT  = '[Project] Select';
+const PROJECT_CLEAR   = '[Project] Clear';
+
+const loadProjects = (state: ProjectsState, projects: Project[]): ProjectsState => {
+  return {
+    projects,
+    currentProject: state.currentProject
+  }
+}
+
+const selectProject = (state: ProjectsState, project: Project): ProjectsState => {
+  return {
+    projects: state.projects,
+    currentProject: project
+  }
+}
+
+const createProject = (state: ProjectsState, project: Project): ProjectsState => {
+  return {
+    projects: [...state.projects, project],
+    currentProject: state.currentProject
+  }
+}
+
+const updateProject = (state: ProjectsState, project: Project): ProjectsState => {
+  return {
+    projects: state.projects.map(p => {
+      return (p.id !== project.id) ? Object.assign({}, project) : p
+    }),
+    currentProject: state.currentProject
+  }
+}
+
+const deleteProject = (state: ProjectsState, project: Project): ProjectsState => {
+  return {
+    projects: state.projects.filter(p => p.id !== project.id),
+    currentProject: null
+  }
+}
+
+const projectsReducer = (state = initialProjectsState, action: Action): ProjectsState => {
+  switch (action.type) {
+    case PROJECT_LOAD:
+      return loadProjects(state, action.payload);
+    case PROJECT_SELECT:
+      return selectProject(state, action.payload);
+    case PROJECT_CREATE:
+      return createProject(state, action.payload);
+    case PROJECT_UPDATE:
+      return updateProject(state, action.payload);
+    case PROJECT_DELETE:
+      return deleteProject(state, action.payload);
+    default:
+      return state;
+  }
+}
+
+class ProjectsStore {
+  state: ProjectsState;
+  reducer
+
+  constructor(state: ProjectsState, reducer) {
+    this.state = state;
+    this.reducer = reducer
+  }
+
+  select(key: string) {
+    return this.state[key];
+  }
+
+  getState(): ProjectsState {
+    return this.state;
+  }
+
+  dispatch(action: Action) {
+    this.state = this.reducer(this.state, action)
+  }
+
+}
+
+const projectsStore = new ProjectsStore(initialProjectsState, projectsReducer);
+const aProject = projectsStore.select('currentProject')
+projectsStore.dispatch({ type: PROJECT_CREATE, payload: hellProject})
+const allProjects = projectsStore.select('projects')
 
 interface AppState {
   clientState: ClientsState;
@@ -96,9 +245,9 @@ interface AppState {
 
 const appState: AppState = {
   clientState: initialClientState,
-  projectState: initialProjectState
+  projectState: initialProjectsState
 }
-const tango = appState
+const tango = allProjects
 
 @Component({
   selector: 'fem-home',
